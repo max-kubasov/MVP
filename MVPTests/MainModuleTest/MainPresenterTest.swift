f6cfbefe5dcd26f7dbad9ef3e9e267c525c406cd
max-kubasov <106583221+max-kubasov@users.noreply.github.com>
2024-06-04 08:11:03 +0300

    Add MainPresenterTest

---
+++ MVPTests/MainModuleTest/MainPresenterTest.swift
@@ -0,0 +1,43 @@
+//
+//  MainPresenterTest.swift
+//  MVPTests
+//
+//  Created by Max on 04.06.2024.
+//
+
+import XCTest
+@testable import MVP
+
+class MockView: MainViewProtocol {
+    var titleTest: String?
+    func setGreeting(greeting: String) {
+        self.titleTest = greeting
+    }
+}
+
+final class MainPresenterTest: XCTestCase {
+    
+    var view: MockView!
+    var person: Person!
+    var presenter: MainPresenter!
+
+    override func setUpWithError() throws {
+        view = MockView()
+        person = Person(firstName: "Baz", lastName: "Bar")
+        presenter = MainPresenter(view: view, person: person)
+    }
+
+    override func tearDownWithError() throws {
+        view = nil
+        person = nil
+        presenter = nil
+    }
+
+    func testModuleIsNotNil() {
+        XCTAssertNotNil(view, "view is not nil")
+    }
+    
+
+
+
+}
